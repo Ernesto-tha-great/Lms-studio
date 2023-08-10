81a68b16f13373a805853aae4e86b14a46b80274
Ernest Nnamdi <Bulaveur@ErnestMackbookAir.local>
2023-08-10 12:20:48 +0100

    first commit

---
+++ src/app/Auth/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/Molecules/NavBar";
+
+const SignUp = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default SignUp;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        /** primary */
+        prosperity: "#FCFF52",
+        forest: "#476520",
+        /** base */
+        gypsum: "#FCF6F1",
+        sand: "#E7E3D4",
+        wood: "#655947",
+        fig: "#1E002B",
+        light: "#ffffff",
+        "light-varient": "#e0eaf6",
+        /** functional */
+        snow: "#FFFFFF",
+        onyx: "#CCCCCC",
+        success: "#329F3B",
+        error: "#E70532",
+        disabled: "#9B9B9B",
+        /** accent */
+        sky: "#7CC0FF",
+        citrus: "#FF9A51",
+        lotus: "#FFA3EB",
+        lavender: "#B490FF",
+      },
+
+      fontFamily: {
+        noto: ["Futura", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
